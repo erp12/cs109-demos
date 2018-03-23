547a6d8f3dc891ab7958fac55b9402431d924515
erp12 <epantridge@massmutual.com>
2018-03-23 10:34:20 -0400

    Added hello world program

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+
+# Adding numbers
+x = 1 + 2 + 3 + 4 + 5
+print(x)
+print()
+
+
+# Equation of a line (y = mx + b)
+slope = 2
+intercept = 3
+x = 1.333
+
+y = intercept + (slope * x)
+print(y)
+
+
+monthly_rent_per_person = 400.00
+num_people = 4
+total_rent = monthly_rent_per_person * num_people
+print(total_rent)
